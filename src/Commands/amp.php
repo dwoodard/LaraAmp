42deb471f2cc3c6f96d630abc4121d88c136ab67
Dustin Woodard <dustin.woodard@gmail.com>
2023-09-16 14:41:27 -0600

    amp command

---
+++ src/Commands/amp.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class amp extends Command
+{
+  /**
+   * The name and signature of the console command.
+   *
+   * @var string
+   */
+  protected $signature = 'amp:install {
+    --force : Force the operation to run.
+  }';
+
+  /**
+   * The console command description.
+   *
+   * @var string
+   */
+  protected $description = 'Command description';
+
+  /**
+   * Execute the console command.
+   */
+  public function handle()
+  {
+    //
+  }
+}
